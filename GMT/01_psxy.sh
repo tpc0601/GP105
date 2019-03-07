7e803618c38ce12da8fb9714528709c6f2db24e6
tpc <bettytpc601@gmail.com>
2019-03-07 19:36:22 +0800

    01_psxy

---
+++ GMT/01_psxy.sh
@@ -0,0 +1,4 @@
+echo 0 0 | gmt psxy -R-100/100/-80/120 -JX10 -Sc1 -Ba20g10/a20g10 -P -K > 01_psxy.ps
+echo 20 20 | gmt psxy -R-100/100/-80/120 -JX10 -Sc1 -Ba20g10/a20g10 -P -O -K >> 01_psxy.ps
+echo -20 -20 | gmt psxy -R-100/100/-80/120 -JX10 -Sc1 -Ba20g10/a20g10 -P -O -K >> 01_psxy.ps
+echo 50 50 | gmt psxy -R-100/100/-80/120 -JX10 -Sc1 -Ba20g10/a20g10 -P -O >> 01_psxy.ps
